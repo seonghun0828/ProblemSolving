33dd32dac235e0f9ecfd19fb9b999e1516b80e09
Seonghun Kim <31424628+seonghun0828@users.noreply.github.com>
2023-02-22 14:54:07 +0900

    [Gold V] Title: 도넛 행성, Time: 532 ms, Memory: 119532 KB -BaekjoonHub

--- 백준/Gold/27211. 도넛 행성/도넛 행성.java
+++ 백준/Gold/27211. 도넛 행성/도넛 행성.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Main {
 	static class Node {
@@ -14,35 +15,33 @@ class Main {
 		}
 	}
 
-	static int N, M;
+	static int N, M, cnt;
 	static int[] dx = { 1, -1, 0, 0 };
 	static int[] dy = { 0, 0, 1, -1 };
-	static boolean[][] isForest;
-	static boolean[][] visited;
+	static int[][] arr;
+	static Queue<Node> q = new LinkedList<>();
 
 	public static void main(String[] args) throws IOException {
 		inputs();
 
 		// 간단한 bfs 문제. 탐색해가며 좌표가 한바퀴 돌아 오는 것만 주의
-		int cnt = 0;
-		visited = new boolean[N][M];
+		cnt = 0;
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (visited[i][j] || isForest[i][j])
+				if (arr[i][j] >= 1) // 방문했으면 arr[i][j]가 1이상임
 					continue;
 
-				visited[i][j] = true;
 				bfs(i, j);
-				cnt++;
 			}
 		}
 		System.out.println(cnt);
 	}
 
 	private static void bfs(int sx, int sy) {
-		Queue<Node> q = new LinkedList<>();
+		q.clear();
 		q.add(new Node(sx, sy));
+		arr[sx][sy] = ++cnt; // arr는 0 or 1이므로 배열의 값에 cnt값을 넣어 방문처리까지 해결
 
 		while (!q.isEmpty()) {
 			Node curr = q.poll();
@@ -52,25 +51,25 @@ class Main {
 				nx = (nx + N) % N; // -1이면 N-1로, N이면 0으로 한바퀴 돌아 좌표 조정
 				ny = (ny + M) % M; // -1이면 M-1로, M이면 0으로 좌표 조정
 
-				if (visited[nx][ny] || isForest[nx][ny])
+				if (arr[nx][ny] >= 1) // 숲이거나 방문했으면
 					continue;
 
-				visited[nx][ny] = true;
 				q.add(new Node(nx, ny));
+				arr[nx][ny] = cnt;
 			}
 		}
 	}
 
 	private static void inputs() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		N = Integer.parseInt(input[0]);
-		M = Integer.parseInt(input[1]);
-		isForest = new boolean[N][M];
+		StringTokenizer stnz = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(stnz.nextToken());
+		M = Integer.parseInt(stnz.nextToken());
+		arr = new int[N][M];
 		for (int i = 0; i < N; i++) {
-			input = br.readLine().split(" ");
+			stnz = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				isForest[i][j] = input[j].equals("1");
+				arr[i][j] = Integer.parseInt(stnz.nextToken());
 			}
 		}
 		br.close();
