2af2c34eac9f776951eeba46da5393f5afdead2a
Seonghun Kim <31424628+seonghun0828@users.noreply.github.com>
2023-02-23 11:59:45 +0900

    [Gold IV] Title: 단어 수학, Time: 2504 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -14,32 +14,32 @@ class Main {
 		inputs();
 
 		ans = 0;
-		sel = new int[alphaCnt];
-		permu(0, 0);
+		sel = new int[alphaCnt]; // 순열 저장할 배열
+		permu(0, 0); // 알파벳에 할당할 숫자들의 순열을 구해 계산하기
 
 		System.out.println(ans);
 	}
 
 	private static void permu(int cnt, int visited) {
-		if (cnt == alphaCnt) {
+		if (cnt == alphaCnt) { // cnt가 알파벳 갯수가 되면 ans 갱신
 			ans = Math.max(ans, getSumFromSel());
 			return;
 		}
 
 		for (int i = 0; i < 10; i++) {
-
 			if ((visited & 1 << i) > 0)
 				continue;
 
-			sel[cnt] = i;
-			permu(cnt + 1, visited | 1 << i);
-			sel[cnt] = 0;
+			sel[cnt] = i; // cnt번째 순열에 i 할당
+			permu(cnt + 1, visited | 1 << i); // cnt + 1, i 방문처리하고 재귀
+			sel[cnt] = 0; // cnt번째 순열에 할당했던 수 0으로 다시 초기화
 		}
 	}
 
 	private static int getSumFromSel() {
 		int sum = 0;
 
+		// 현재 순열에서 총합 구하기
 		for (String word : words) {
 			int num = 0;
 			for (int i = 0; i < word.length(); i++) {
