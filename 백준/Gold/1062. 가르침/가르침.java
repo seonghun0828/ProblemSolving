1637600a4b52b1e8f274dcb8008c1fae68ceaf93
Seonghun Kim <31424628+seonghun0828@users.noreply.github.com>
2023-05-16 12:27:35 +0900

    [Gold IV] Title: 가르침, Time: 548 ms, Memory: 293184 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 class Main {
 	static int N, K, alphaLen, ans;
-	static boolean[] alphabet;
 	static String[] words;
 
 	public static void main(String[] args) throws IOException {
@@ -20,43 +19,43 @@ class Main {
 		}
 
 		alphaLen = 26;
-		alphabet = new boolean[alphaLen];
+		int taught = 0;
 		char[] basicAlpha = new char[] { 'a', 'n', 't', 'i', 'c' };
-		for (char basic : basicAlpha) { // 기본 문자 alphabet 배열에 반영
-			alphabet[basic - 'a'] = true;
+		for (char basic : basicAlpha) { // 기본 문자들을 비트마스킹 taught에 반영
+			int alphaIdx = basic - 'a';
+			taught |= 1 << alphaIdx;
 		}
 
-		recur(0, 5); // idx 0, cnt 5에서 시작해 재귀
+		recur(0, 5, taught); // idx 0, cnt 5에서 시작해 재귀
 
 		return ans;
 	}
 
-	private static void recur(int idx, int cnt) {
+	private static void recur(int idx, int cnt, int taught) {
 		if (cnt == K) { // 가르친 글자 수가 K개가 되면 재귀 종료
-			int teachableWordCnt = getTeachableWordCnt(); // 가르친 글자로 읽을 수 있는 단어 숫자
+			int teachableWordCnt = getTeachableWordCnt(taught); // 가르친 글자로 읽을 수 있는 단어 숫자
 			ans = Math.max(ans, teachableWordCnt);
 			return;
 		}
 
 		for (int i = idx; i < alphaLen; i++) { // 시작 idx부터 26까지 반복
-			if (alphabet[i]) { // 이미 가르친 글자
+			if ((taught & 1 << i) > 0) { // 이미 가르친 글자
 				continue;
 			}
 
-			alphabet[i] = true; // i번째 글자를 가르침
-			recur(i + 1, cnt + 1); // i번째 글자 가르치고 재귀
-			alphabet[i] = false; // i번째 글자 가르친거 취소
+			recur(i + 1, cnt + 1, taught | 1 << i); // i번째 글자 가르치고 재귀
 		}
 	}
 
-	private static int getTeachableWordCnt() {
+	private static int getTeachableWordCnt(int taught) {
 		int cnt = 0;
 
 		for (String ele : words) {
 			boolean isReadable = true;
 			char[] word = ele.toCharArray();
 			for (char c : word) {
-				if (!alphabet[c - 'a']) { // 단어의 한 글자가 알파벳에 없음
+				int cIdx = c - 'a';
+				if ((taught & 1 << cIdx) == 0) { // 단어의 한 글자가 taught 비트마스킹에 없음
 					isReadable = false;
 					break;
 				}
