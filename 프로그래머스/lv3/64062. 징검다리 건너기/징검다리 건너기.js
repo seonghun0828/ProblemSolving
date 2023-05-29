c9283ac387a2bbe2a521afc21c44ce2453683ac3
Seonghun Kim <31424628+seonghun0828@users.noreply.github.com>
2023-05-29 09:56:19 +0900

    [level 3] Title: 징검다리 건너기, Time: 6.50 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.js
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -16,16 +16,16 @@ function solution(stones, k) {
     return ans;
 }
 
-function canCross(stones, k, mid) {
-    let cnt = 0;
+function canCross(stones, k, friendsCnt) {  // 
+    let cnt = 0; // 한 번에 뛰어야 하는 징검다리 길이
     for (let i = 0; i < stones.length; i++) {
-        if (stones[i] <= mid) {
-            cnt++;
-            if (cnt >= k) {
+        if (stones[i] <= friendsCnt) { // 현재 디딤돌 숫자보다 친구들 수가 작거나 같음
+            cnt++; // 현재 디딤돌 사라졌으니 한 번에 뛰어야 하는 징검다리 길이 + 1
+            if (cnt >= k) { // 한 번에 뛰어야 하는 징검다리 길이가 k 이상이면 불가능
                 return false;
             }
-        } else {
-            cnt = 0;
+        } else {  // 친구들 수로 현재 디딤돌 다 건너도 디딤돌이 사라지지 않음
+            cnt = 0;  // 현재 디딤돌에서 한 번에 뛰어야 하는 징검다리 길이는 0으로 초기화
         }
     }
     return true;
